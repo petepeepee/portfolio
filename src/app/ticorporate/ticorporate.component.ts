ced3375df98167acc8f4e13bcf3a59465995ad4d
Petri Paasila <ad1782@student.jamk.fi>
2024-12-31 09:58:31 +0200

    kuvia lisätty yms

--- src/app/ticorporate/ticorporate.component.ts
+++ src/app/ticorporate/ticorporate.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-ticorporate',
   templateUrl: './ticorporate.component.html',
-  styleUrls: ['./ticorporate.component.css']
+  styleUrls: ['./ticorporate.component.css'],
 })
-export class TicorporateComponent {
-
-}
+export class TicorporateComponent {}
